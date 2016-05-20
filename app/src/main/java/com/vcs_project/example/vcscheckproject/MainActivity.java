9844ca49b3b9f00bfe74cbeea91bee5943e6dc4a
rashid <rashid@unitedwereach.org>
2016-05-20 12:05:58 -0700

    second change for sample heelo world

--- app/src/main/java/com/vcs_project/example/vcscheckproject/MainActivity.java
+++ app/src/main/java/com/vcs_project/example/vcscheckproject/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+//        new changes for git commit
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
